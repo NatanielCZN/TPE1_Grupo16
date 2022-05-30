07840a28a798b88c094f8925b3392476938d7287
Nataniel <natanielarg@gmail.com>
2022-05-30 14:58:07 -0300

    Ligeros cambios en las clases de Candidato

--- TPE1_Grupo16/src/main/java/ar/edu/unju/fi/tpe1/controller/CandidatoController.java
+++ TPE1_Grupo16/src/main/java/ar/edu/unju/fi/tpe1/controller/CandidatoController.java
@@ -92,7 +92,7 @@ public class CandidatoController {
 	 */
 	@GetMapping("/editar/{codigo}")
 	public ModelAndView getEditarDatosCandidatoPage(@PathVariable(value="codigo") String codigo) {
-		ModelAndView modelAV = new ModelAndView("edicion_docente");
+		ModelAndView modelAV = new ModelAndView("edicion_candidato");
 		Candidato candidato = candidatoService.buscarCandidato(codigo);
 		modelAV.addObject("candidato", candidato);
 		
