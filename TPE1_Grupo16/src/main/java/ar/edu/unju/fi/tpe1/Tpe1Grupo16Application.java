511fbb7f000f2aef20c2b2d11521d952ad4c9b12
Nataniel <natanielarg@gmail.com>
2022-05-30 04:08:25 -0300

    primer commit

---
+++ TPE1_Grupo16/src/main/java/ar/edu/unju/fi/tpe1/Tpe1Grupo16Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpe1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpe1Grupo16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpe1Grupo16Application.class, args);
+	}
+
+}
---
+++ TPE1_Grupo16/src/test/java/ar/edu/unju/fi/tpe1/Tpe1Grupo16ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpe1Grupo16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
